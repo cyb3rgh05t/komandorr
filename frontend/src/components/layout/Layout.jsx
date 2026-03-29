a0a26c181338cd20547696b34d232260fe600ad9
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2026-03-29 16:26:13 +0200

    list view test

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -7,7 +7,9 @@ export default function Layout({ children }) {
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0">
         <TopNavbar />
-        <main className="flex-1 overflow-x-hidden overflow-y-auto">{children}</main>
+        <main className="flex-1 overflow-x-hidden overflow-y-auto">
+          {children}
+        </main>
       </div>
     </div>
   );
